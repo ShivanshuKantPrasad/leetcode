969e75ef40ef13ddcd3f95e85a5df5f492a0e9b1
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2024-04-29 10:01:43 +0530

    Leetcode Day 05

---
+++ day05/xor.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int minOperations(int *nums, int numsSize, int k) {
+  int xor = 0;
+  for (int i = 0; i < numsSize; i++) {
+    xor = xor^nums[i];
+  }
+
+  xor = xor^k;
+  return __builtin_popcount(xor);
+}
+
+int main() {
+  int nums[] = {2, 1, 3, 4};
+  printf("%d", minOperations(nums, 4, 1));
+  return 0;
+}
