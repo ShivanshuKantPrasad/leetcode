e5a8f15b7d50f28d810ad139ef614c6481882547
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2024-04-30 10:42:36 +0530

    Leetcode Day 06

---
+++ day06/awesome.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+long long wonderfulSubstrings(char *word) {
+  int cnt[1025] = {0};
+  cnt[0] = 1;
+
+  char *letter = word;
+  int i = 0;
+  int mask = 0;
+  long long res = 0;
+  while (*letter != '\0') {
+    mask ^= (1 << (*letter - 'a'));
+    res += cnt[mask];
+    for (int i = 0; i < 10; i++) {
+      res += cnt[mask ^ (1 << i)];
+    }
+    cnt[mask]++;
+    letter++;
+    i++;
+  }
+
+  return res;
+}
+
+int main() {
+  printf("%lld\n", wonderfulSubstrings("aba"));
+  printf("%lld\n", wonderfulSubstrings("aabb"));
+  printf("%lld\n", wonderfulSubstrings("he"));
+}
