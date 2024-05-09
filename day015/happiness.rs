4f6af0c08b8d589cec0b4aff9115de4c8046e6d7
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2024-05-09 08:48:14 +0530

    Add Day 015

---
+++ day015/happiness.rs
@@ -0,0 +1,19 @@
+struct Solution;
+impl Solution {
+    pub fn maximum_happiness_sum(happiness: Vec<i32>, k: i32) -> i64 {
+        let mut happiness = happiness;
+        happiness.sort_unstable_by_key(|&x| -x);
+        happiness
+            .iter()
+            .enumerate()
+            .map(|(i, x)| (x - i as i32).max(0) as i64)
+            .take(k as usize)
+            .sum()
+    }
+}
+
+fn main() {
+    println!("{}", Solution::maximum_happiness_sum(vec![1, 2, 3], 2));
+    println!("{}", Solution::maximum_happiness_sum(vec![1, 1, 1, 1], 2));
+    println!("{}", Solution::maximum_happiness_sum(vec![2, 3, 4, 5], 1));
+}
