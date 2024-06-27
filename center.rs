a415f670e230290bdeb3dab693396668b9e92ea7
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2024-06-27 18:35:58 +0530

    Add day 065

---
+++ center.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-center-of-star-graph/
+
+pub fn find_center(edges: Vec<Vec<i32>>) -> i32 {
+    if let Some(x) = edges[1].iter().position(|x| *x == edges[0][0]) {
+        edges[1][x]
+    } else {
+        edges[0][1]
+    }
+}
