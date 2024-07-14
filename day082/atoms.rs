79dd687495e6f66fd923d6f02530f649dee8e31d
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2024-07-14 09:11:15 +0530

    Refactor

--- day082/atoms.rs
+++ day082/atoms.rs
@@ -3,7 +3,7 @@
 pub fn count_of_atoms(formula: String) -> String {
     use std::collections::HashMap;
 
-    fn count_of_atoms(formula: &[u8]) -> HashMap<String, i32> {
+    fn parse_atoms(formula: &[u8]) -> HashMap<String, i32> {
         let mut freq = HashMap::new();
         let mut i = 0;
         while i < formula.len() {
@@ -27,14 +27,14 @@ pub fn count_of_atoms(formula: String) -> String {
                 }
                 let end = i;
                 i += 1;
-                let req = count_of_atoms(&formula[start..end]);
+                let sub_freq = parse_atoms(&formula[start..end]);
                 let mut mult = 0;
                 while i < formula.len() && (formula[i] as char).is_numeric() {
                     mult = mult * 10 + (formula[i] - b'0') as i32;
                     i += 1;
                 }
                 mult = mult.max(1);
-                for (key, val) in req.iter() {
+                for (key, val) in sub_freq.iter() {
                     *freq.entry(key.clone()).or_insert(0) += val * mult;
                 }
             } else if (formula[i] as char).is_uppercase() {
@@ -50,15 +50,14 @@ pub fn count_of_atoms(formula: String) -> String {
                     i += 1;
                 }
                 mult = mult.max(1);
-                let key = formula[start..end].to_vec();
-                let key = String::from_utf8(key).unwrap();
+                let key = String::from_utf8(formula[start..end].to_vec()).unwrap();
                 *freq.entry(key).or_insert(0) += mult;
             }
         }
         freq
     }
 
-    let freq = count_of_atoms(formula.as_bytes());
+    let freq = parse_atoms(formula.as_bytes());
     // println!("{freq:?}");
     let mut freq = freq.iter().collect::<Vec<_>>();
     // println!("{freq:?}");
