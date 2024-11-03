48ab516d2af9da686ba4b34188f66aa564466535
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2024-11-03 18:50:52 +0530

    Add day 193

--- day193/rotate.rs
+++ day193/rotate.rs
@@ -1,6 +1,7 @@
 // https://leetcode.com/problems/rotate-string/description/
 
 pub fn rotate_string(s: String, goal: String) -> bool {
+    // s.len() == goal.len() && (s.clone() + &s).contains(&goal)
     s.len() == goal.len()
         && (0..s.len()).any(|i| {
             s.chars()
