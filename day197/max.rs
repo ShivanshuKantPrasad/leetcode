1d827f17820e66aac86fa71d92ceb5a59364cea9
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2024-11-07 19:55:15 +0530

    Add day 197

--- day197/max.rs
+++ day197/max.rs
@@ -1,15 +1,20 @@
 // https://leetcode.com/problems/largest-combination-with-bitwise-and-greater-than-zero/submissions/1445822505/
 
 pub fn largest_combination(candidates: Vec<i32>) -> i32 {
-    let mut bits = [0; 24];
+    // let mut bits = [0; 24];
+    //
+    // for num in candidates {
+    //     for i in 0..24 {
+    //         if (1 << i) & num != 0 {
+    //             bits[i] += 1;
+    //         }
+    //     }
+    // }
+    //
+    // bits.into_iter().max().unwrap_or(0)
 
-    for num in candidates {
-        for i in 0..24 {
-            if (1 << i) & num != 0 {
-                bits[i] += 1;
-            }
-        }
-    }
-
-    bits.into_iter().max().unwrap_or(0)
+    (0..24)
+        .map(|i| candidates.iter().filter(|&num| (1 << i) & num != 0).count())
+        .max()
+        .unwrap() as i32
 }
