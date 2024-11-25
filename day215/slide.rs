1c56a86aceec0460ddb487d9f26cd1c9008ebf60
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2024-11-25 11:34:31 +0530

    Add day 215

--- day215/slide.rs
+++ day215/slide.rs
@@ -3,7 +3,7 @@
 pub fn sliding_puzzle(board: Vec<Vec<i32>>) -> i32 {
     use std::collections::{HashSet, VecDeque};
 
-    let directions = vec![
+    let directions = [
         vec![1, 3],
         vec![0, 2, 4],
         vec![1, 5],
@@ -13,12 +13,12 @@ pub fn sliding_puzzle(board: Vec<Vec<i32>>) -> i32 {
     ];
 
     let target = "123450";
-    let mut start_state = board
+    let start_state = board
         .iter()
         .flatten()
         .map(|x| x.to_string())
-        .collect::<Vec<String>>()
-        .join("");
+        .collect::<String>();
+
     if start_state == target {
         return 0;
     }
@@ -26,15 +26,15 @@ pub fn sliding_puzzle(board: Vec<Vec<i32>>) -> i32 {
     let mut visited = HashSet::new();
     let mut queue = VecDeque::new();
     queue.push_back((start_state.clone(), 0));
-    visited.insert(start_state.clone());
+    visited.insert(start_state);
 
     while let Some((current_state, moves)) = queue.pop_front() {
         let zero_pos = current_state.find('0').unwrap();
 
         for &new_pos in &directions[zero_pos] {
-            let mut temp = current_state.clone().into_bytes();
+            let mut temp = current_state.as_bytes().to_vec();
             temp.swap(zero_pos, new_pos);
-            let next_state = String::from_utf8(temp.to_vec()).unwrap();
+            let next_state = String::from_utf8(temp).unwrap();
 
             if next_state == target {
                 return moves + 1;
