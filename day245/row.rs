8ed98ae473e0a9aa02ee068546b160fc6ebce7ec
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2024-12-25 13:29:50 +0530

    Add day 245

---
+++ day245/row.rs
@@ -0,0 +1,57 @@
+// https://leetcode.com/problems/find-largest-value-in-each-tree-row/description/
+use std::cell::RefCell;
+use std::rc::Rc;
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+pub fn largest_values(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    use std::collections::VecDeque;
+
+    let mut queue = VecDeque::new();
+    let mut res = Vec::<i32>::new();
+
+    let mut last_row = 0;
+
+    match root {
+        Some(root) => {
+            queue.push_back((root.clone(), 0));
+            res.push(i32::MIN);
+            while let Some((root, cur_row)) = queue.pop_front() {
+                let root = root.borrow();
+                if cur_row == last_row {
+                    res[cur_row] = res[cur_row].max(root.val);
+                } else {
+                    res.push(root.val);
+                    last_row = cur_row;
+                }
+                match &root.left {
+                    Some(left) => queue.push_back((left.clone(), cur_row + 1)),
+                    None => (),
+                }
+                match &root.right {
+                    Some(right) => queue.push_back((right.clone(), cur_row + 1)),
+                    None => (),
+                }
+            }
+            res
+        }
+        None => vec![],
+    }
+}
